d924cb2db6af2846660518ba50fcaa55df4a4718
Kara Anderson <kanderson8585@gmail.com>
2023-03-23 17:17:29 -0600

    refac:  formatted numbers and genres

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -6,16 +6,16 @@ const MovieDetails = ({selectedMovie, chooseMovie}) => {
     <div className="overlay-single-movie">
       <img src={selectedMovie.backdrop_path} className="backdrop"/>
       <div className="movie-info">
-        <img src={selectedMovie.poster_path} class="movie-poster" />
+        <img src={selectedMovie.poster_path} className="movie-poster" />
         <div className="details">
           <h2>{selectedMovie.title} - {Math.round(selectedMovie.average_rating)}/10</h2>
           <ul>
-            {selectedMovie.overview && <li>Overview: {selectedMovie.overview}</li>}
-            {selectedMovie.genres && <li>Genres: {selectedMovie.genres}</li>}
-            {selectedMovie.budget && <li>Budget: {selectedMovie.budget}</li>}
-            {selectedMovie.revenue && <li>Revenue: {selectedMovie.revenue}</li>}
-            {selectedMovie.runtime && <li>Runtime: {selectedMovie.runtime}</li>}
-            {selectedMovie.tagline && <li>Tagline: {selectedMovie.tagline}</li>}
+            {selectedMovie.overview ? <li><span className="category">Overview:</span> {selectedMovie.overview}</li> : null}
+            {selectedMovie.genres ? <li><span className="category">Genres:</span> {selectedMovie.genres.map((genre, index) => index ? ", " + genre : genre)}</li>: null}
+            {selectedMovie.budget ? <li><span className="category">Budget:</span> ${selectedMovie.budget.toLocaleString()}</li>: null}
+            {selectedMovie.revenue ? <li><span className="category">Revenue:</span> ${selectedMovie.revenue.toLocaleString()}</li>: null}
+            {selectedMovie.runtime ? <li><span className="category">Runtime:</span> {selectedMovie.runtime} minutes</li>: null}
+            {selectedMovie.tagline ? <li><span className="category">Tagline:</span> {selectedMovie.tagline}</li>: null}
           </ul>
         </div>
       </div>
