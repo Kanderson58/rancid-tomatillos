e03ab14193c507453786d63ed12d5a944c0f4be2
Lauren Seward <lauren.unal@gmail.com>
2023-03-24 16:43:20 -0500

    Fix: search bar functionality to render all Movies when cleared

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import "./Header.css"
 import tomatillo from "./tomatillo.png"
+import SearchBar from "./SearchBar/SearchBar"
 
-const Header = () => {
+const Header = ({value, onChange}) => {
   return (
     <header>
       <h1>Rancid Tomatillos</h1>
       <img className="tomatillo" src={tomatillo} alt="tomatillo" />
+      <SearchBar value={value} onChange={onChange} />
     </header>
   )
 }
