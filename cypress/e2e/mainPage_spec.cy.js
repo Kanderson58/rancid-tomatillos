336d8c111ee013a896bd076a2065099cf5983dbe
Lauren Seward <lauren.unal@gmail.com>
2023-03-30 16:28:45 -0500

    Update tests for main page and change css for header/trademark

--- cypress/e2e/mainPage_spec.cy.js
+++ cypress/e2e/mainPage_spec.cy.js
@@ -1,23 +1,27 @@
 describe('Main Page', () => {
   beforeEach(() => {
-    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/')
+    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      fixture: 'movies.json'
+    })
     .visit('http://localhost:3000/')
   });
 
-  it('should have a header with the title "Rancid Tomatillos" and a tomatillo image', () => {
-    cy.get('header').should('exist');
-    cy.get('h1').should('have.text', 'Rancid Tomatillos');
-    cy.get('.tomatillo').should('exist');
+  it('should have a header with the title "Rancid Tomatillos", a tomatillo image, and trademark', () => {
+    cy.get('header').should('exist')
+    .get('h1').should('have.text', 'Rancid Tomatillos')
+    .get('.tomatillo').should('exist')
+    .get('.trademark').should('have.text', 'Definitely Not Rotten Tomatoes ™️')
+    
   });
 
   it('Should display all movie cards in the movie list with movie posters', () => {
-    cy.get('.movieCard').find("img").should('be.visible').should('have.length', 40)
+    cy.get('.movieCard').find("img").should('be.visible').should('have.length', 2)
   });
 
   it('should display the correct movie image in each movie card', () => {
     cy.get('.movieCard').first().within(() => {
-      cy.get('img').should('have.attr', 'src', 'https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg');
-      cy.get('.title').should('have.text', 'Black Adam');
+      cy.get('img').should('have.attr', 'src', 'https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg')
+      .get('.title').should('have.text', 'Black Adam');
     });
   });
 
@@ -38,12 +42,20 @@ describe('Main Page', () => {
     cy.url().should('not.eq', 'http://localhost:3000/')
   });
 
+  it('Should display error message for 404 status code', () => {
+    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 404
+    })
+    cy.visit('http://localhost:3000/')
+      .contains('Sorry, something went wrong: Error: Not Found')
+  })
+
   it('Should display error message for 500 status code', () => {
     cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
       statusCode: 500
     })
     cy.visit('http://localhost:3000/')
-      .contains('Sorry, there was an error loading your page!')
+      .contains('Sorry, something went wrong: Error: Internal Server Error')
   })
   
 });
