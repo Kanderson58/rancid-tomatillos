b840b1c241f26dbc6712e438b65a21d97fd72434
Lauren Seward <lauren.unal@gmail.com>
2023-03-31 09:33:08 -0500

    Refac: remove logic from return statement

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -15,46 +15,67 @@ class MovieDetails extends Component {
     }
   }
 
-  componentDidMount = () => {
+  componentDidMount() {
     getMovieById(this.props.selectedMovieID)
-    .then(data => this.setState({ movieDetails: data.movie, loading: false}))
-    .catch(error => this.setState({ error: error.toString() }))
+    .then(data => {
+      const { movie } = data;
+      this.setState({ 
+        movieDetails: movie, 
+        loading: false 
+      });
+    })
+    .catch(error => {
+      this.setState({ error: error.toString() });
+    });
+  }
+
+  renderLoading() {
+    return (
+      <p className="loading">Loading...</p>
+    );
+  }
+
+  renderError() {
+    return (
+      <Error error={this.state.error} chooseMovie={this.props.chooseMovie}/>
+    );
+  }
+
+  renderMovieDetails() {
+    const { movieDetails } = this.state;
+
+    return (
+      <div className="overlay-single-movie">
+        <img src={movieDetails.backdrop_path} className="backdrop" alt={movieDetails.title}/>
+        <div className="movie-info">
+          <img src={movieDetails.poster_path} className="movie-poster" alt={movieDetails.title} />
+          <div className="details">
+            <h2>{movieDetails.title} {movieDetails.tagline && <em>- "<span>{movieDetails.tagline}</span>"</em>}</h2>
+            <ul>
+              {movieDetails.average_rating ? <li><span className="category">Rating:</span> {movieDetails.average_rating}/10</li> : null}
+              {movieDetails.overview && <li><span className="category">Overview:</span> {movieDetails.overview}</li>}
+              {movieDetails.genres && <li><span className="category">Genres:</span> {movieDetails.genres.map((genre, index) => index ? ", " + genre : genre)}</li>}
+              {movieDetails.budget ? <li><span className="category">Budget:</span> ${movieDetails.budget.toLocaleString()}</li>: null}
+              {movieDetails.revenue ? <li><span className="category">Revenue:</span> ${movieDetails.revenue.toLocaleString()}</li>: null}
+              {movieDetails.runtime ? <li><span className="category">Runtime:</span> {movieDetails.runtime} minutes</li>: null}
+            </ul>
+          </div>
+        </div>
+        <Footer chooseMovie={this.props.chooseMovie} />
+      </div>
+    );
   }
 
   render() {
-    const movie = this.state.movieDetails;
-
-    if(this.state.loading && !this.state.error) {
-      return (<p className="loading">Loading...</p>)
+    if (this.state.loading && !this.state.error) {
+      return this.renderLoading();
     } else if (this.state.error) {
-      return <Error error={this.state.error} chooseMovie={this.props.chooseMovie}/>
+      return this.renderError();
+    } else {
+      return this.renderMovieDetails();
     }
-    return (
-
-    <div className="overlay-single-movie">
-      <img src={movie.backdrop_path} className="backdrop" alt={movie.title}/>
-      <div className="movie-info">
-        <img src={movie.poster_path} className="movie-poster" alt={movie.title} />
-        <div className="details">
-          <h2>{movie.title} {movie.tagline && <em>- "<span>{movie.tagline}</span>"</em>}</h2>
-          <ul>
-            {movie.average_rating ? <li><span className="category">Rating:</span> {movie.average_rating}/10</li> : null}
-            {movie.overview && <li><span className="category">Overview:</span> {movie.overview}</li>}
-            {movie.genres && <li><span className="category">Genres:</span> {movie.genres.map((genre, index) => index ? ", " + genre : genre)}</li>}
-            {movie.budget ? <li><span className="category">Budget:</span> ${movie.budget.toLocaleString()}</li>: null}
-            {movie.revenue ? <li><span className="category">Revenue:</span> ${movie.revenue.toLocaleString()}</li>: null}
-            {movie.runtime ? <li><span className="category">Runtime:</span> {movie.runtime} minutes</li>: null}
-          </ul>
-        </div>
-      </div>
-      <Footer chooseMovie={this.props.chooseMovie} />
-    </div>
-    )
   }
 }
-
-export default MovieDetails;
-
 
 MovieDetails.propTypes = {
   chosenMovie: PropTypes.shape({
@@ -65,4 +86,6 @@ MovieDetails.propTypes = {
     release_date: PropTypes.string,
     title: PropTypes.string
   }).isRequired
-}
+};
+
+export default MovieDetails;
