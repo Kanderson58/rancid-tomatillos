214037e24e47ff077e6df4de6b9015c62ae1e647
Lauren Seward <lauren.unal@gmail.com>
2023-03-31 09:35:42 -0500

    Refac: remove logic from error return statement'

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -2,13 +2,20 @@ import './Error.css'
 import Footer from '../MovieDetails/Footer/Footer'
 
 const Error = ({error, chooseMovie}) => {
+  let errorMessage;
+  if (error) {
+    errorMessage = <p className="error">Sorry, something went wrong: {error}</p>;
+  } else {
+    errorMessage = <p className="error">Sorry, something went wrong: that page does not exist.</p>;
+  }
+
   return (
     <div>
-    {error && <p className="error">Sorry, something went wrong: {error}</p>}
-    {!error && <p className="error">Sorry, something went wrong: that page does not exist.</p>}
-    <Footer chooseMovie={chooseMovie} />
+      {errorMessage}
+      <Footer chooseMovie={chooseMovie} />
     </div>
   )
 }
+
 
 export default Error
